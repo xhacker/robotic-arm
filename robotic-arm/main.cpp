2199081696d3e37f1a4a8eb22c00bfa781f2e48c
Xhacker Liu <liu.dongyuan@gmail.com>
2014-03-08 15:52:20 -0800

    Initial Commit

---
+++ robotic-arm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  robotic-arm
+//
+//  Created by Xhacker Liu on 3/8/14.
+//  Copyright (c) 2014 Xhacker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
