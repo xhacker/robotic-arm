862fc13b60fc4f5bad4213666c60d5c0fecd5335
Xhacker Liu <liu.dongyuan@gmail.com>
2014-03-08 22:52:55 -0800

    Add switch view menu item. Reformat.

--- robotic-arm/main.cpp
+++ robotic-arm/main.cpp
@@ -17,74 +17,85 @@ point4 points[NumVertices];
 color4 colors[NumVertices];
 
 point4 vertices[8] = {
-    point4( -0.5, -0.5,  0.5, 1.0 ),
-    point4( -0.5,  0.5,  0.5, 1.0 ),
-    point4(  0.5,  0.5,  0.5, 1.0 ),
-    point4(  0.5, -0.5,  0.5, 1.0 ),
-    point4( -0.5, -0.5, -0.5, 1.0 ),
-    point4( -0.5,  0.5, -0.5, 1.0 ),
-    point4(  0.5,  0.5, -0.5, 1.0 ),
-    point4(  0.5, -0.5, -0.5, 1.0 )
+    point4(-0.5, -0.5, 0.5, 1.0), point4(-0.5, 0.5, 0.5, 1.0),
+    point4(0.5, 0.5, 0.5, 1.0), point4(0.5, -0.5, 0.5, 1.0),
+    point4(-0.5, -0.5, -0.5, 1.0), point4(-0.5, 0.5, -0.5, 1.0),
+    point4(0.5, 0.5, -0.5, 1.0), point4(0.5, -0.5, -0.5, 1.0)
 };
 
 // RGBA olors
-color4 vertex_colors[8] = {
-    color4( 0.0, 0.0, 0.0, 1.0 ),  // black
-    color4( 1.0, 0.0, 0.0, 1.0 ),  // red
-    color4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-    color4( 0.0, 1.0, 0.0, 1.0 ),  // green
-    color4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-    color4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-    color4( 1.0, 1.0, 1.0, 1.0 ),  // white
-    color4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+color4 vertex_colors[8] = { color4(0.0, 0.0, 0.0, 1.0), // black
+                            color4(1.0, 0.0, 0.0, 1.0), // red
+                            color4(1.0, 1.0, 0.0, 1.0), // yellow
+                            color4(0.0, 1.0, 0.0, 1.0), // green
+                            color4(0.0, 0.0, 1.0, 1.0), // blue
+                            color4(1.0, 0.0, 1.0, 1.0), // magenta
+                            color4(1.0, 1.0, 1.0, 1.0), // white
+                            color4(0.0, 1.0, 1.0, 1.0) // cyan
 };
 
-
 // Parameters controlling the size of the Robot's arm
-const GLfloat BASE_HEIGHT      = 2.0;
-const GLfloat BASE_WIDTH       = 5.0;
+const GLfloat BASE_HEIGHT = 2.0;
+const GLfloat BASE_WIDTH = 5.0;
 const GLfloat LOWER_ARM_HEIGHT = 5.0;
-const GLfloat LOWER_ARM_WIDTH  = 0.5;
+const GLfloat LOWER_ARM_WIDTH = 0.5;
 const GLfloat UPPER_ARM_HEIGHT = 5.0;
-const GLfloat UPPER_ARM_WIDTH  = 0.5;
+const GLfloat UPPER_ARM_WIDTH = 0.5;
 
 // Shader transformation matrices
-mat4  model_view;
+mat4 model_view;
 GLuint ModelView, Projection;
 
 // Array of rotation angles (in degrees) for each rotation axis
-enum { Base = 0, LowerArm = 1, UpperArm = 2, NumAngles = 3 };
-int      Axis = Base;
-GLfloat  Theta[NumAngles] = { 0.0 };
+enum {
+    Base = 0,
+    LowerArm = 1,
+    UpperArm = 2,
+    NumAngles = 3
+};
+int Axis = Base;
+GLfloat Theta[NumAngles] = { 0.0 };
 
 // Menu option values
-const int  Quit = 4;
+const int Quit = 4;
+const int SwitchView = 5;
 
+bool is_side_view = true;
 
 //----------------------------------------------------------------------------
 
 int Index = 0;
 
-void
-quad( int a, int b, int c, int d )
+void quad(int a, int b, int c, int d)
 {
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[b]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[c]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[c]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[d]; Index++;
+    colors[Index] = vertex_colors[a];
+    points[Index] = vertices[a];
+    Index++;
+    colors[Index] = vertex_colors[a];
+    points[Index] = vertices[b];
+    Index++;
+    colors[Index] = vertex_colors[a];
+    points[Index] = vertices[c];
+    Index++;
+    colors[Index] = vertex_colors[a];
+    points[Index] = vertices[a];
+    Index++;
+    colors[Index] = vertex_colors[a];
+    points[Index] = vertices[c];
+    Index++;
+    colors[Index] = vertex_colors[a];
+    points[Index] = vertices[d];
+    Index++;
 }
 
-void
-colorcube()
+void colorcube()
 {
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
 }
 
 //----------------------------------------------------------------------------
@@ -95,64 +106,49 @@ colorcube()
  rotation, translation, and scaling to create instances
  of symbols (cube and cylinder */
 
-void
-base()
+void base()
 {
-    mat4 instance = ( Translate( 0.0, 0.5 * BASE_HEIGHT, 0.0 ) *
-                     Scale( BASE_WIDTH,
-                           BASE_HEIGHT,
-                           BASE_WIDTH ) );
+    mat4 instance = (Translate(0.0, 0.5 * BASE_HEIGHT, 0.0) * Scale(BASE_WIDTH, BASE_HEIGHT, BASE_WIDTH));
 
-    glUniformMatrix4fv( ModelView, 1, GL_TRUE, model_view * instance );
+    glUniformMatrix4fv(ModelView, 1, GL_TRUE, model_view * instance);
 
-    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 }
 
 //----------------------------------------------------------------------------
 
-void
-upper_arm()
+void upper_arm()
 {
-    mat4 instance = ( Translate( 0.0, 0.5 * UPPER_ARM_HEIGHT, 0.0 ) *
-                     Scale( UPPER_ARM_WIDTH,
-                           UPPER_ARM_HEIGHT,
-                           UPPER_ARM_WIDTH ) );
+    mat4 instance = (Translate(0.0, 0.5 * UPPER_ARM_HEIGHT, 0.0) * Scale(UPPER_ARM_WIDTH, UPPER_ARM_HEIGHT, UPPER_ARM_WIDTH));
 
-    glUniformMatrix4fv( ModelView, 1, GL_TRUE, model_view * instance );
-    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    glUniformMatrix4fv(ModelView, 1, GL_TRUE, model_view * instance);
+    glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 }
 
 //----------------------------------------------------------------------------
 
-void
-lower_arm()
+void lower_arm()
 {
-    mat4 instance = ( Translate( 0.0, 0.5 * LOWER_ARM_HEIGHT, 0.0 ) *
-                     Scale( LOWER_ARM_WIDTH,
-                           LOWER_ARM_HEIGHT,
-                           LOWER_ARM_WIDTH ) );
+    mat4 instance = (Translate(0.0, 0.5 * LOWER_ARM_HEIGHT, 0.0) * Scale(LOWER_ARM_WIDTH, LOWER_ARM_HEIGHT, LOWER_ARM_WIDTH));
 
-    glUniformMatrix4fv( ModelView, 1, GL_TRUE, model_view * instance );
-    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    glUniformMatrix4fv(ModelView, 1, GL_TRUE, model_view * instance);
+    glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 }
 
 //----------------------------------------------------------------------------
 
-void
-display( void )
+void display(void)
 {
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Accumulate ModelView Matrix as we traverse the tree
-    model_view = RotateY(Theta[Base] );
+    model_view = RotateY(Theta[Base]);
     base();
 
-    model_view *= ( Translate(0.0, BASE_HEIGHT, 0.0) *
-                   RotateZ(Theta[LowerArm]) );
+    model_view *= (Translate(0.0, BASE_HEIGHT, 0.0) * RotateZ(Theta[LowerArm]));
     lower_arm();
 
-    model_view *= ( Translate(0.0, LOWER_ARM_HEIGHT, 0.0) *
-                   RotateZ(Theta[UpperArm]) );
+    model_view *= (Translate(0.0, LOWER_ARM_HEIGHT, 0.0) * RotateZ(Theta[UpperArm]));
     upper_arm();
 
     glutSwapBuffers();
@@ -160,64 +156,65 @@ display( void )
 
 //----------------------------------------------------------------------------
 
-void
-init( void )
+void init(void)
 {
     colorcube();
 
     // Create a vertex array object
     GLuint vao;
-    glGenVertexArraysAPPLE( 1, &vao );
-    glBindVertexArrayAPPLE( vao );
+    glGenVertexArraysAPPLE(1, &vao);
+    glBindVertexArrayAPPLE(vao);
 
     // Create and initialize a buffer object
     GLuint buffer;
-    glGenBuffers( 1, &buffer );
-    glBindBuffer( GL_ARRAY_BUFFER, buffer );
-    glBufferData( GL_ARRAY_BUFFER, sizeof(points) + sizeof(colors),
-                 NULL, GL_DYNAMIC_DRAW );
-    glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(points), points );
-    glBufferSubData( GL_ARRAY_BUFFER, sizeof(points), sizeof(colors), colors );
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points) + sizeof(colors), NULL,
+                 GL_DYNAMIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(points), points);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(points), sizeof(colors), colors);
 
     // Load shaders and use the resulting shader program
-    GLuint program = InitShader( "vshader81.glsl", "fshader81.glsl" );
-    glUseProgram( program );
+    GLuint program = InitShader("vshader.glsl", "fshader.glsl");
+    glUseProgram(program);
 
-    GLuint vPosition = glGetAttribLocation( program, "vPosition" );
-    glEnableVertexAttribArray( vPosition );
-    glVertexAttribPointer( vPosition, 4, GL_FLOAT, GL_FALSE, 0,
-                          BUFFER_OFFSET(0) );
+    GLuint vPosition = glGetAttribLocation(program, "vPosition");
+    glEnableVertexAttribArray(vPosition);
+    glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
 
-    GLuint vColor = glGetAttribLocation( program, "vColor" );
-    glEnableVertexAttribArray( vColor );
-    glVertexAttribPointer( vColor, 4, GL_FLOAT, GL_FALSE, 0,
-                          BUFFER_OFFSET(sizeof(points)) );
+    GLuint vColor = glGetAttribLocation(program, "vColor");
+    glEnableVertexAttribArray(vColor);
+    glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0,
+                          BUFFER_OFFSET(sizeof(points)));
 
-    ModelView = glGetUniformLocation( program, "ModelView" );
-    Projection = glGetUniformLocation( program, "Projection" );
+    ModelView = glGetUniformLocation(program, "ModelView");
+    Projection = glGetUniformLocation(program, "Projection");
 
-    glEnable( GL_DEPTH );
-    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+    glEnable(GL_DEPTH);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    glClearColor( 1.0, 1.0, 1.0, 1.0 );
+    glClearColor(1.0, 1.0, 1.0, 1.0);
 }
 
 //----------------------------------------------------------------------------
 
-void
-mouse( int button, int state, int x, int y )
+void mouse(int button, int state, int x, int y)
 {
 
-    if ( button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
         // Incrase the joint angle
         Theta[Axis] += 5.0;
-        if ( Theta[Axis] > 360.0 ) { Theta[Axis] -= 360.0; }
+        if (Theta[Axis] > 360.0) {
+            Theta[Axis] -= 360.0;
+        }
     }
 
-    if ( button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN ) {
+    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
         // Decrase the joint angle
         Theta[Axis] -= 5.0;
-        if ( Theta[Axis] < 0.0 ) { Theta[Axis] += 360.0; }
+        if (Theta[Axis] < 0.0) {
+            Theta[Axis] += 360.0;
+        }
     }
 
     glutPostRedisplay();
@@ -225,11 +222,19 @@ mouse( int button, int state, int x, int y )
 
 //----------------------------------------------------------------------------
 
-void
-menu( int option )
+void menu(int option)
 {
-    if ( option == Quit ) {
-        exit( EXIT_SUCCESS );
+    if (option == Quit) {
+        exit(EXIT_SUCCESS);
+    }
+    else if (option == SwitchView) {
+        if (is_side_view) {
+            glutChangeToMenuEntry(4, "top view", SwitchView);
+        }
+        else {
+            glutChangeToMenuEntry(4, "side view", SwitchView);
+        }
+        is_side_view = !is_side_view;
     }
     else {
         Axis = option;
@@ -238,18 +243,17 @@ menu( int option )
 
 //----------------------------------------------------------------------------
 
-void
-reshape( int width, int height )
+void reshape(int width, int height)
 {
-    glViewport( 0, 0, width, height );
+    glViewport(0, 0, width, height);
 
-    GLfloat  left = -10.0, right = 10.0;
-    GLfloat  bottom = -5.0, top = 15.0;
-    GLfloat  zNear = -10.0, zFar = 10.0;
+    GLfloat left = -10.0, right = 10.0;
+    GLfloat bottom = -5.0, top = 15.0;
+    GLfloat zNear = -10.0, zFar = 10.0;
 
-    GLfloat aspect = GLfloat(width)/height;
+    GLfloat aspect = GLfloat(width) / height;
 
-    if ( aspect > 1.0 ) {
+    if (aspect > 1.0) {
         left *= aspect;
         right *= aspect;
     }
@@ -258,50 +262,50 @@ reshape( int width, int height )
         top /= aspect;
     }
 
-    mat4 projection = Ortho( left, right, bottom, top, zNear, zFar );
-    glUniformMatrix4fv( Projection, 1, GL_TRUE, projection );
+    mat4 projection = Ortho(left, right, bottom, top, zNear, zFar);
+    glUniformMatrix4fv(Projection, 1, GL_TRUE, projection);
 
-    model_view = mat4( 1.0 );  // An Identity matrix
+    model_view = mat4(1.0); // An Identity matrix
 }
 
 //----------------------------------------------------------------------------
 
-void
-keyboard( unsigned char key, int x, int y )
+void keyboard(unsigned char key, int x, int y)
 {
-    switch( key ) {
-        case 033: // Escape Key
-        case 'q': case 'Q':
-            exit( EXIT_SUCCESS );
-            break;
+    switch (key) {
+    case 033: // Escape Key
+    case 'q':
+    case 'Q':
+        exit(EXIT_SUCCESS);
+        break;
     }
 }
 
 //----------------------------------------------------------------------------
 
-int
-main( int argc, char **argv )
+int main(int argc, char** argv)
 {
-    glutInit( &argc, argv );
-    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
-    glutInitWindowSize( 512, 512 );
-    glutCreateWindow( "robot" );
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(512, 512);
+    glutCreateWindow("robot");
 
     init();
 
-    glutDisplayFunc( display );
-    glutReshapeFunc( reshape );
-    glutKeyboardFunc( keyboard );
-    glutMouseFunc( mouse );
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
 
-    glutCreateMenu( menu );
+    glutCreateMenu(menu);
     // Set the menu values to the relevant rotation axis values (or Quit)
-    glutAddMenuEntry( "base", Base );
-    glutAddMenuEntry( "lower arm", LowerArm );
-    glutAddMenuEntry( "upper arm", UpperArm );
-    glutAddMenuEntry( "quit", Quit );
-    glutAttachMenu( GLUT_MIDDLE_BUTTON );
-    
+    glutAddMenuEntry("base", Base);
+    glutAddMenuEntry("lower arm", LowerArm);
+    glutAddMenuEntry("upper arm", UpperArm);
+    glutAddMenuEntry("side view", SwitchView);
+    glutAddMenuEntry("quit", Quit);
+    glutAttachMenu(GLUT_MIDDLE_BUTTON);
+
     glutMainLoop();
     return 0;
 }
